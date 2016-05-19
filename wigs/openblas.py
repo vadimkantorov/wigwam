6652e97570e5dbc39621a4f0dae99db65e84d577
Vadim Kantorov <vadimkantorov@gmail.com>
2016-05-19 02:44:38 +0200

    Update openblas.py

--- wigs/openblas.py
+++ wigs/openblas.py
@@ -1,8 +1,12 @@
 class openblas(Wig):
 	tarball_uri = 'https://github.com/xianyi/OpenBLAS/archive/v$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v0.2.18'
+	supported_features = ['debug']
 
 	def setup(self):
 		self.skip('configure')
 		self.make_flags += ['FC=gfortran', 'NO_AFFINITY=1', 'USE_OPENMP=1']
 		self.make_install_flags += [S.PREFIX_MAKE_INSTALL_FLAG]
+	
+	def switch_debug_on(self):
+		self.make_flags += ['DEBUG=1']
