2f0c13979eb211ad368f8a9b11c525d114ecbf56
Vadim Kantorov <vadimkantorov@gmail.com>
2016-06-14 00:49:52 +0200

    Update x264.py

--- wigs/x264.py
+++ wigs/x264.py
@@ -2,4 +2,4 @@ class x264(Wig):
   git_uri = 'http://git.videolan.org/git/x264.git'
   
   def setup(self):
-    self.configure_flags += ['--enable-static']
+    self.configure_flags += ['--enable-static', '--enable-shared']
