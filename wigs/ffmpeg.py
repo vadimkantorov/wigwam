027a980092e6c4b007ca2c804563411d8f7272a1
Vadim Kantorov <vadimkantorov@gmail.com>
2016-06-13 23:41:31 +0200

    Update ffmpeg.py

--- wigs/ffmpeg.py
+++ wigs/ffmpeg.py
@@ -1,10 +1,14 @@
 class ffmpeg(Wig):
 	tarball_uri = 'http://ffmpeg.org/releases/ffmpeg-$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v3.0.1'
-	optional_dependencies = ['yasm']
-	supported_features = ['yasm']
+	optional_dependencies = ['yasm', 'x264']
+	supported_features = ['yasm', 'x264']
 	default_features = ['+yasm']
 
 	def switch_yasm_on(self):
 		self.require('yasm')
 		self.configure_flags += ['--yasmexe="$PREFIX/bin/yasm"', '--enable-shared']
+
+	def switch_x264_on(self):
+		self.require('x264')
+		self.configure_flags += ['--enable-gpl', '--enable-libx264']
