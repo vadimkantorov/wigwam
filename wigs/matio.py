feb0c2411f3946a1a1222bfd63f1bc8c9ec46fa1
Vadim Kantorov <vadimkantorov@gmail.com>
2016-07-12 16:11:29 +0200

    Update matio.py

--- wigs/matio.py
+++ wigs/matio.py
@@ -1,4 +1,4 @@
-class matio(Wig):
+class matio(AutogenWig):
   tarball_uri = 'http://downloads.sourceforge.net/project/matio/matio/$RELEASE_VERSION$/matio-$RELEASE_VERSION$.tar.gz'
   git_uri = 'https://github.com/tbeu/matio'
   last_release_version = 'v1.5.8'
