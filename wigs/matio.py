09d5a11ed8decf2d6aa832e78780d015c23c4d43
Vadim Kantorov <vadimkantorov@gmail.com>
2016-07-12 16:08:21 +0200

    Update matio.py

--- wigs/matio.py
+++ wigs/matio.py
@@ -1,4 +1,5 @@
 class matio(Wig):
   tarball_uri = 'http://downloads.sourceforge.net/project/matio/matio/$RELEASE_VERSION$/matio-$RELEASE_VERSION$.tar.gz'
+  git_uri = 'https://github.com/tbeu/matio'
   last_release_version = 'v1.5.8'
   dependencies = ['zlib', 'hdf5']
