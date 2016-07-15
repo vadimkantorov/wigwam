83a1f074f7836d9ed4a8211d011223b1e5fca298
Vadim Kantorov <vadimkantorov@gmail.com>
2016-07-16 00:43:42 +0200

    Update caffe.py

--- wigs/caffe.py
+++ wigs/caffe.py
@@ -8,47 +8,54 @@ class caffe(Wig):
 	
 	def setup(self):
 		self.skip('make parallel')
-
-		self.python_dirs += [os.path.join(self.paths.src_dir, 'python')]
 		self.config_fixes = ''
 		
-	def switch_matlab_on(self):
-		self.config_fixes += '''| sed 's$# MATLAB_DIR$MATLAB_DIR := %s# MATLAB_DIR$' ''' % os.path.join(os.path.dirname(self.cfg('PATH_TO_MATLAB')), '..')
+	def add_config_fix(var_name, var_value):
+		self.config_fixes += '''| sed 's$# %s :=$%s := %s# %s :=$' ''' % (var_name, var_name, var_value, var_name)
+
+	def add_config_fix_dirs(var_name, var_prepend):
+		self.config_fixes += '''| sed 's$%s :=$%s := %s $' ''' % (var_name, var_name, var_prepend)
 
 	def switch_openblas_on(self):
 		self.require('openblas')
-		self.config_fixes += '''| sed 's$BLAS := atlas$BLAS := open# BLAS := atlas$' '''
-		self.config_fixes += '''| sed 's$# BLAS_INCLUDE := /path/to/your/blas$BLAS_INCLUDE := '$PREFIX'/include# BLAS_INCLUDE := /path/to/your/blas$' '''
-		self.config_fixes += '''| sed 's$# BLAS_LIB := /path/to/your/blas$BLAS_LIB := '$PREFIX'/lib# BLAS_LIB := /path/to/your/blas$' '''
+		self.add_config_fix('BLAS', 'open')
+		self.add_config_fix('BLAS_INCLUDE', "'$PREFIX'/include")
+		self.add_config_fix('BLAS_LIB', "'$PREFIX'/lib")
 		
 	def switch_opencv(self, on):
 		if on:
 			self.require('opencv')
-			self.config_fixes += '''| sed 's$# OPENCV_VERSION$OPENCV_VERSION := 3# OPENCV_VERSION$' '''
-		self.config_fixes += '''| sed 's$# USE_OPENCV$USE_OPENCV := %d# USE_OPENCV$' ''' % (1 if on else 0)
+			self.add_config_fix('OPENCV_VERSION', 3)
+		self.add_config_fix('USE_OPENCV', 1 if on else 0)
 		
 	def switch_lmdb(self, on):
 		if on:
 			self.require('lmdb')
-		self.config_fixes += '''| sed 's$# USE_LMDB$USE_LMDB := %d# USE_LMDB$' ''' % (1 if on else 0)
+		self.add_config_fix('USE_LMDB', 1 if on else 0)
 		
 	def switch_leveldb(self, on):
 		if on:
 			self.require('leveldb')
-		self.config_fixes += '''| sed 's$# USE_LEVELDB$USE_LEVELDB := %d# USE_LEVELDB$' ''' % (1 if on else 0)
-
+		self.add_config_fix('USE_LEVELDB', 1 if on else 0)
+		
 	def switch_cudnn_on(self):
 		self.lib_dirs += [os.path.dirname(self.cfg('PATH_TO_CUDNN_SO'))]
-		self.include_dirs += [os.path.join(os.path.dirname(self.cfg('PATH_TO_CUDNN_SO')), '../include')]
-		self.config_fixes += '''| sed 's$# USE_CUDNN$USE_CUDNN := 1# USE_CUDNN$' '''
+		self.add_config_fix('USE_CUDNN', 1)
+		self.add_config_fix_dirs('LIBRARY_DIRS', os.path.dirname(self.cfg('PATH_TO_CUDNN_SO')))
+		self.add_config_fix_dirs('INCLUDE_DIRS', os.path.join(os.path.dirname(self.cfg('PATH_TO_CUDNN_SO')), '../include'))
 		
 	def switch_cuda_on(self):
 		self.lib_dirs += [os.path.join(os.path.dirname(self.cfg('PATH_TO_NVCC')), '../lib64')]
-		self.config_fixes += '''| sed 's$CUDA_DIR := /usr/local/cuda$CUDA_DIR := %s#CUDA_DIR := /usr/local/cuda$' ''' % os.path.join(os.path.dirname(self.cfg('PATH_TO_NVCC')), '..')
-	
+		self.add_config_fix('CUDA_DIR', os.path.join(os.path.dirname(self.cfg('PATH_TO_NVCC')), '..')
+
+	def switch_matlab_on(self):
+		self.after_make += [S.make(self.make_flags + ['matcaffe'])]
+		self.add_config_fix('MATLAB_DIR', os.path.join(os.path.dirname(self.cfg('PATH_TO_MATLAB')), '..'))
+		
 	def switch_python_on(self):
+		self.python_dirs += [os.path.join(self.paths.src_dir, 'python')]
 		self.after_make += [S.make(self.make_flags + ['pycaffe'])]
-		self.config_fixes += '''| sed 's$# WITH_PYTHON_LAYER$WITH_PYTHON_LAYER := 1# WITH_PYTHON_LAYER$' '''
+		self.add_config_fix('WITH_PYTHON_LAYER', 1)
 
 	def gen_configure_snippet(self):
 		return ['cat Makefile.config.example %s > Makefile.config' % self.config_fixes]
