fa1df25b7877e6a49873b6809b13e5f22a75b28e
Vadim Kantorov <vadimkantorov@gmail.com>
2016-08-12 09:58:01 +0200

    Upgraded hdf5 version

--- wigs/hdf5.py
+++ wigs/hdf5.py
@@ -1,3 +1,3 @@
 class hdf5(Wig):
 	tarball_uri = 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-$RELEASE_VERSION$/src/hdf5-$RELEASE_VERSION$.tar.gz'
-	last_release_version = 'v1.8.16'
+	last_release_version = 'v1.8.17'
