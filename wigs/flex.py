203379563e7005115e6b622ac61120c991cff94a
Vadim Kantorov <vadimkantorov@gmail.com>
2016-09-08 17:01:02 +0200

    Delete flex.py

--- wigs/flex.py
+++
@@ -1,5 +0,0 @@
-class flex(AutogenWig):
-	tarball_uri = 'http://downloads.sourceforge.net/project/flex/flex-$RELEASE_VERSION$.tar.gz'
-	git_uri = 'https://github.com/westes/flex'
-	dependencies = ['gettext']
-	last_release_version = 'v2.5.39'
