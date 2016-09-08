ccb01020b746c2062a39fc58162c44f9ecbd05e5
Vadim Kantorov <vadimkantorov@gmail.com>
2016-09-08 15:27:50 +0200

    Delete fbtorch.py

--- wigs/fbtorch.py
+++
@@ -1,4 +0,0 @@
-class fbtorch(LuarocksWig):
-	git_uri = 'https://github.com/facebook/fbtorch.git'
-	rockspec_path = 'rocks/fbtorch-scm-1.rockspec'
-	dependencies = ['torch', 'fblualib', 'fbnn', 'fbcunn']
