3df4137c16ba18f6b43fd76542c25b1b1be0d983
Vadim Kantorov <vadimkantorov@gmail.com>
2016-09-10 16:48:25 +0200

    Delete jemalloc.py

--- wigs/jemalloc.py
+++
@@ -1,4 +0,0 @@
-class jemalloc(Wig):
-	git_uri = 'https://github.com/jemalloc'
-	tarball_uri = 'https://github.com/jemalloc/jemalloc/releases/download/$RELEASE_VERSION$/jemalloc-$RELEASE_VERSION$.tar.bz2'
-	last_release_version = 'v3.6.0'
