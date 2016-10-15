218d04f205e39410c0ddb29eaeaf3c5f5d0be0b9
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-15 17:16:17 +0200

    Update snappy.py

--- wigs/snappy.py
+++ wigs/snappy.py
@@ -1,7 +1,7 @@
 class snappy(Wig):
-	tarball_uri = 'http://snappy.googlecode.com/files/snappy-$RELEASE_VERSION$.tar.gz'
+	tarball_uri = 'https://github.com/google/snappy/releases/download/$RELEASE_VERSION/snappy-$RELEASE_VERSION$.tar.gz'
 	git_uri = 'https://github.com/google/snappy'
-	last_release_version = 'v1.1.1'
+	last_release_version = 'v1.1.3'
 
 	def setup(self):
 		self.before_make += [S.export('CXXFLAGS', '-fPIC -shared')]
