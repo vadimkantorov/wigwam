2261529d38001596d7a44ad5c5d0c3c5f0b08d7e
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-15 17:17:35 +0200

    Update snappy.py

--- wigs/snappy.py
+++ wigs/snappy.py
@@ -1,5 +1,5 @@
 class snappy(Wig):
-	tarball_uri = 'https://github.com/google/snappy/releases/download/$RELEASE_VERSION/snappy-$RELEASE_VERSION$.tar.gz'
+	tarball_uri = 'https://github.com/google/snappy/releases/download/$RELEASE_VERSION$/snappy-$RELEASE_VERSION$.tar.gz'
 	git_uri = 'https://github.com/google/snappy'
 	last_release_version = 'v1.1.3'
 
