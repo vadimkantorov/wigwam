a1d11addca4a820b406e3f4546be02537cf02d3f
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-15 16:36:01 +0200

    Update boost.py

--- wigs/boost.py
+++ wigs/boost.py
@@ -5,13 +5,14 @@ class boost(Wig):
 	git_init_submodules = True
 	last_release_version = 'v1.59.0'
 	supported_features = ['python']
+	default_features = ['-python']
 	
 	def setup(self):
 		self.skip('make')
-		self.bootstrap_flags = ['--without-libraries=python']
-		
-	def switch_python_on(self):
 		self.bootstrap_flags = []
+		
+	def switch_python(self, on):
+		self.bootstrap_flags = [] if on else ['--without-libraries=python']
 
 	def gen_install_snippet(self):
 		return ['./bootstrap.sh ' + ' '.join(self.bootstrap_flags), './b2 %s install %s' % (' '.join(self.make_flags), S.PREFIX_CONFIGURE_FLAG)]	
