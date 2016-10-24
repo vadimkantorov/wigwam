2b4ef3044b5359d0883c6988710476957a6f3f6e
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-24 20:22:13 +0200

    Update git.py

--- wigs/git.py
+++ wigs/git.py
@@ -2,3 +2,6 @@ class git(Wig):
 	tarball_uri = 'https://github.com/git/git/archive/v$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v2.10.1'
 	git_uri = 'https://github.com/git/git'
+
+	def setup(self):
+		self.before_configure += [S.make(['configure'])]
