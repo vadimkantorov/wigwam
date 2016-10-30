0fb791a2397e926715baa6fcc4638c4b361a9ee5
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-30 19:43:59 +0100

    Update libedit.py

--- wigs/libedit.py
+++ wigs/libedit.py
@@ -2,5 +2,5 @@ class libedit(Wig):
 	tarball_uri = 'http://thrysoee.dk/editline/libedit-$RELEASE_VERSION$.tar.gz'		
 	last_release_version = 'v20150325-3.1'		
 			
-	def setup(self):		
-		self.configure_flags += ['--disable-shared']
+	#def setup(self):		
+	#	self.configure_flags += ['--disable-shared']
