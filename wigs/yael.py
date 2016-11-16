ff9171c41d9b758bb97da8f5d8c10b96467f4a85
Vadim Kantorov <vadimkantorov@gmail.com>
2016-11-16 19:39:21 +0100

    Update yael.py

--- wigs/yael.py
+++ wigs/yael.py
@@ -2,7 +2,7 @@ class yael(Wig):
 	tarball_uri = 'https://gforge.inria.fr/frs/download.php/file/33810/yael_v$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v438'
 	supported_features = ['python', 'atlas']
-	optional_dependencies = ['atlas']
+	optional_dependencies = ['atlas', 'swig']
 
 	def setup(self):
 		self.skip('make install', 'prefix')
@@ -11,6 +11,7 @@ class yael(Wig):
 		return 'bash configure.sh %s' % ' '.join(self.configure_flags)
 
 	def switch_python_on(self):
+		self.require('swig')
 		self.configure_flags += ['--enable-numpy']
 	
 	def switch_atlas_on(self):
