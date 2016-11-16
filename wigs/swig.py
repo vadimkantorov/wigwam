6992a12acb5c0e6c0b62c4a939989075fd6df2ff
Vadim Kantorov <vadimkantorov@gmail.com>
2016-11-16 19:33:25 +0100

    Update swig.py

--- wigs/swig.py
+++ wigs/swig.py
@@ -1,4 +1,4 @@
-class swig(Wig):
+class swig(AutogenWig):
 	tarball_uri = 'https://github.com/swig/swig/archive/rel-$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v3.0.10'
 	git_uri = 'https://github.com/swig/swig'
