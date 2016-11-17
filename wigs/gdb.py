cee09a2d53f96779351145bc1e51a231500212bf
Vadim Kantorov <vadimkantorov@gmail.com>
2016-11-17 22:17:59 +0100

    Update gdb.py

--- wigs/gdb.py
+++ wigs/gdb.py
@@ -1,4 +1,4 @@
 class gdb(Wig):
 	tarball_uri = 'http://ftp.gnu.org/gnu/gdb/gdb-$RELEASE_VERSION$.tar.gz'
-	last_release_version = 'v7.10.1'
+	last_release_version = 'v7.12'
 	dependencies = ['texinfo']
