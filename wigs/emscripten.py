f145ddb1fab78bc85b8e998ccfabd92e58bf06fc
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-28 19:53:36 +0100

    Fixed emscripten wig

--- wigs/emscripten.py
+++ wigs/emscripten.py
@@ -5,6 +5,6 @@ class emscripten(CmakeWig):
 
 	def setup(self):
 		self.after_fetch += [S.fetch_git('https://github.com/kripken/emscripten-fastcomp', os.path.join(self.paths.src_dir, self.working_directory)),
-			S.fetch_git('https://github.com/kripken/emscripten-fastcomp-clang', os.path.join(self.paths.src_dir, self.working_directory, 'tools', 'clang')
+			S.fetch_git('https://github.com/kripken/emscripten-fastcomp-clang', os.path.join(self.paths.src_dir, self.working_directory, 'tools', 'clang'))
 		]
 		self.cmake_flags += ['-DCMAKE_BUILD_TYPE=Release', '-DLLVM_TARGETS_TO_BUILD="X86;JSBackend"', '-DLLVM_INCLUDE_EXAMPLES=OFF', '-DLLVM_INCLUDE_TESTS=OFF', '-DCLANG_INCLUDE_EXAMPLES=OFF', '-DCLANG_INCLUDE_TESTS=OFF']
