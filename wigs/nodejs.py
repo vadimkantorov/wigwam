836c7aa92cd9d35e7d54046e835f285410780b84
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-28 19:18:02 +0100

    Create nodejs.py

---
+++ wigs/nodejs.py
@@ -0,0 +1,4 @@
+class nodejs(Wig):
+  git_uri = 'https://github.com/nodejs/node'
+  tarball_uri = 'https://github.com/nodejs/node/archive/v$RELEASE_VERSION$.tar.gz'
+  last_release_version = 'v4.7.2'
