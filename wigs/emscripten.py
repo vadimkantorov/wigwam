6ccbe193594d0517ce1414622ec43df2bf20b0ed
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-28 19:49:53 +0100

    Fixed emscripten wig

--- wigs/emscripten.py
+++ wigs/emscripten.py
@@ -1,6 +1,10 @@
-class emscripten(Wig):
+class emscripten(CmakeWig):
 	git_uri = 'https://github.com/kripken/emscripten'
-	dependencies = ['cmake', 'nodejs', 'deb-default-jre']
+	dependencies = ['nodejs', 'deb-default-jre']
+	working_directory = 'emscripten-fastcomp'
 
 	def setup(self):
-		self.skip('make')
+		self.after_fetch += [S.fetch_git('https://github.com/kripken/emscripten-fastcomp', os.path.join(self.paths.src_dir, self.working_directory)),
+			S.fetch_git('https://github.com/kripken/emscripten-fastcomp-clang', os.path.join(self.paths.src_dir, self.working_directory, 'tools', 'clang')
+		]
+		self.configure_flags += ['--enable-optimized', '--disable-assertions', '--enable-targets=host,js']
