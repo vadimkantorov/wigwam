17013d530849a03dec9ee5ff110f955ac089a94b
Vadim Kantorov <vadimkantorov@gmail.com>
2017-02-22 16:45:26 +0900

    Update zlib.py

--- wigs/zlib.py
+++ wigs/zlib.py
@@ -1,3 +1,4 @@
 class zlib(Wig):
-	tarball_uri = 'http://zlib.net/zlib-$RELEASE_VERSION$.tar.gz'
+	tarball_uri = 'https://github.com/madler/zlib/archive/v$RELEASE_VERSION$.tar.gz'
+	git_uri = 'https://github.com/madler/zlib'
 	last_release_version = 'v1.2.11'
