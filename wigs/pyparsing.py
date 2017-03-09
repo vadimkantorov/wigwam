93f223b9c24544357f8ed724bbf1ec74de9909dc
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:32:13 +0900

    Delete pyparsing.py

--- wigs/pyparsing.py
+++
@@ -1,3 +0,0 @@
-class pyparsing(PythonWig):
-	tarball_uri = 'https://sourceforge.net/projects/pyparsing/files/pyparsing/pyparsing-$RELEASE_VERSION$/pyparsing-$RELEASE_VERSION$.tar.gz'
-	last_release_version = 'v2.2.0'
