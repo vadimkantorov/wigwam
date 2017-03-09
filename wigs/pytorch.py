342837c874a2c0aa592fdf926dc6b6fc141a0558
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:37:11 +0900

    Update pytorch.py

--- wigs/pytorch.py
+++ wigs/pytorch.py
@@ -1,6 +1,6 @@
 class pytorch(PythonWig):
 	git_uri = 'https://github.com/pytorch/pytorch'
-	dependencies = ['numpy', 'cmake', 'pip-pyyaml', 'pip-cffi', 'setuptools']
+	dependencies = ['numpy', 'cmake', 'magma'] #, 'pip-pyyaml', 'pip-cffi', 'setuptools']
 	supported_features = ['cuda']
 
 	def switch_cuda_off(self):
