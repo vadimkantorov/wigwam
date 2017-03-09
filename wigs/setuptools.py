eeca15c506d896f96a1cb187032656a397664510
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:22:53 +0900

    Update setuptools.py

--- wigs/setuptools.py
+++ wigs/setuptools.py
@@ -1,5 +1,8 @@
 class setuptools(PythonWig):
 	git_uri = 'https://github.com/pypa/setuptools'
 	tarball_uri = 'https://github.com/pypa/setuptools/archive/v$RELEASE_VERSION$.tar.gz'
-	dependencies = ['pypa', 'pyparsing', 'appdirs']
+	dependencies = ['pypa', 'pyparsing', 'appdirs', 'runwiththis']
 	last_release_version = 'v34.3.1'
+	
+	def setup(self):
+		self.before_install += ['rwt -- bootstrap.py']
