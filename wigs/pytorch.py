d5b1c06197b1044323b28d33d1d169670aa50bfb
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 21:21:11 +0900

    Update pytorch.py

--- wigs/pytorch.py
+++ wigs/pytorch.py
@@ -1,3 +1,7 @@
 class pytorch(PythonWig):
 	git_uri = 'https://github.com/pytorch/pytorch'
 	dependencies = ['numpy', 'cmake', 'pip-pyyaml', 'pip-cffi']
+	supported_features = ['cuda']
+
+	def switch_cuda_off(self):
+		self.before_install += [S.export('NO_CUDA', '1')]
