3a45db64b5e1650bb51264759eac7741eb007787
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:38:41 +0900

    Update pytorch.py

--- wigs/pytorch.py
+++ wigs/pytorch.py
@@ -1,7 +1,12 @@
 class pytorch(PythonWig):
 	git_uri = 'https://github.com/pytorch/pytorch'
-	dependencies = ['numpy', 'cmake', 'magma'] #, 'pip-pyyaml', 'pip-cffi', 'setuptools']
+	dependencies = ['numpy', 'cmake'] #, 'pip-pyyaml', 'pip-cffi', 'setuptools']
+	optional_dependencies = ['magma']
 	supported_features = ['cuda']
+	default_features = ['cuda']
 
-	def switch_cuda_off(self):
-		self.before_install += [S.export('NO_CUDA', '1')]
+	def switch_cuda(self, on):
+		if on:
+			self.require('magma')
+		else:
+			self.before_install += [S.export('NO_CUDA', '1')]
