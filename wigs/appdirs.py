a8085fe6a9150f214de1938092baaa982bbfef98
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:05:09 +0900

    Create appdirs.py

---
+++ wigs/appdirs.py
@@ -0,0 +1,4 @@
+class appdirs(PythonWig):
+	git_uri = 'http://github.com/ActiveState/appdirs'
+	tarball_uri = 'https://github.com/ActiveState/appdirs/archive/$RELEASE_VERSION$.tar.gz'
+	last_release_version = 'v1.4.3'
