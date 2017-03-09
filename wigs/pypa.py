6f38bf818012c95dfb5a4dfde31055245b396147
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 21:42:21 +0900

    Create pypa.py

---
+++ wigs/pypa.py
@@ -0,0 +1,4 @@
+class pypa(PythonWig):
+	git_uri = 'https://github.com/pypa/packaging'
+	tarball_uri = 'https://github.com/pypa/packaging/archive/$RELEASE_VERSION$.tar.gz'
+	last_release_version = '16.8'
