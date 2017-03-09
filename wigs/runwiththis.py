e615b355cd418b86f3ac908e3216fff0a8092ebc
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:32:56 +0900

    Delete runwiththis.py

--- wigs/runwiththis.py
+++
@@ -1,4 +0,0 @@
-class runwiththis(PythonWig):
-	git_uri = 'https://github.com/jaraco'
-	tarball_uri = 'https://github.com/jaraco/rwt/archive/$RELEASE_VERSION$.tar.gz'
-	last_release_version = 'v2.14'
