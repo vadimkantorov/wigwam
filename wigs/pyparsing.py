74be8ee67a418e2b8c3fd72d37bb2ff1c3747395
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 21:47:52 +0900

    Create pyparsing.py

---
+++ wigs/pyparsing.py
@@ -0,0 +1,3 @@
+class pyparsing(PythonWig):
+	tarball_uri = 'https://sourceforge.net/projects/pyparsing/files/pyparsing/pyparsing-$RELEASE_VERSION$/pyparsing-$RELEASE_VERSION$.tar.gz'
+	last_release_version = 'v2.2.0'
