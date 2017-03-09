9c3fd5311bc1f07e2e73cdc9ececd5d8da745178
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 22:21:59 +0900

    Create runwiththis.py

---
+++ wigs/runwiththis.py
@@ -0,0 +1,4 @@
+class runwiththis(PythonWig):
+	git_uri = 'https://github.com/jaraco'
+	tarball_uri = 'https://github.com/jaraco/rwt/archive/$RELEASE_VERSION$.tar.gz'
+	last_release_version = 'v2.14'
