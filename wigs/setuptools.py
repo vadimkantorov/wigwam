b861dbf01eac344053398f44e6b75ad084b86ba1
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-09 21:26:51 +0900

    Create setuptools.py

---
+++ wigs/setuptools.py
@@ -0,0 +1,4 @@
+class setuptools(PythonWig):
+	git_uri = 'https://github.com/pypa/setuptools'
+	tarball_uri = 'https://github.com/pypa/setuptools/archive/v$RELEASE_VERSION$.tar.gz'
+	last_release_version = 'v34.3.1'
