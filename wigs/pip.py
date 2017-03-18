bc548149026f894522247a0eeb649a5d5e7187f4
Vadim Kantorov <vadimkantorov@gamil.com>
2017-03-18 22:01:57 +0900

    @

--- wigs/pip.py
+++ wigs/pip.py
@@ -6,7 +6,7 @@ class pip(Wig):
 		self.skip('configure', 'make')
 
 	def gen_fetch_snippet(self):
-		return [S.mkdir_p(self.paths.src_dir), P.download(self.raw_uri, os.path.join(self.paths.src_dir, os.path.basename(self.raw_uri))]
+		return [S.mkdir_p(self.paths.src_dir), P.download(self.raw_uri, os.path.join(self.paths.src_dir, os.path.basename(self.raw_uri)))]
 	
 	def gen_install_snippet(self):
 		return ['python "%s" pip setuptools wheel' % os.path.join(self.paths.src_dir, os.path.basename(self.raw_uri))]
