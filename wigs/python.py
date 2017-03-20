0434e245b6ba0ce9633f4e1894fd60f2697e93f1
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-20 16:12:22 +0900

    Create python.py

---
+++ wigs/python.py
@@ -0,0 +1,6 @@
+class python(Wig):
+	git_uri = 'https://github.com/python/cpython'
+	tarball_uri = 'https://github.com/python/cpython/archive/v$RELEASE_VERSION$.tar.gz'
+	last_release_version = 'v2.7.13'
+	
+	dependencies = ['openssl']
