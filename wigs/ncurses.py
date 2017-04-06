2b85083240486cd417db6782ad9c808041674f05
Vadim Kantorov <vadimkantorov@gmail.com>
2017-04-06 20:56:50 +0900

    Update ncurses.py

--- wigs/ncurses.py
+++ wigs/ncurses.py
@@ -3,5 +3,6 @@ class ncurses(Wig):
 	last_release_version = 'v6.0'
 
 	def setup(self):
+		self.configure_flags += ['--enable-overwrite']
 		self.before_configure += [S.export(S.CFLAGS, '-fPIC'), S.export(S.CPPFLAGS, '-P')]
 		self.before_make += [S.export(S.CFLAGS, '-fPIC')]
