90592fd0edc752d92c5880963e0339913f9f6cec
Vadim Kantorov <vadimkantorov@gmail.com>
2017-04-06 20:16:45 +0900

    Update python.py

--- wigs/python.py
+++ wigs/python.py
@@ -3,4 +3,4 @@ class python(Wig):
 	tarball_uri = 'https://github.com/python/cpython/archive/v$RELEASE_VERSION$.tar.gz'
 	last_release_version = 'v2.7.13'
 	
-	dependencies = ['openssl']
+	dependencies = ['openssl', 'ncurses']
