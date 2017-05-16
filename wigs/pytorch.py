415514b4fb1e584072d1bc49516e3691e41a131d
Vadim Kantorov <vadimkantorov@gmail.com>
2017-05-16 15:01:00 +0200

    Update pytorch.py

--- wigs/pytorch.py
+++ wigs/pytorch.py
@@ -4,6 +4,8 @@ class pytorch(PythonWig):
 	optional_dependencies = ['magma']
 	supported_features = ['cuda']
 	default_features = ['+cuda']
+	
+	# TODO: set env CUDA_HOME for custom CUDA path
 
 	def switch_cuda(self, on):
 		if on:
