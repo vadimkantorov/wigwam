3d02a060864aade9ac247ed7247cf7cf265b67fc
Vadim Kantorov <vadimkantorov@gmail.com>
2017-05-31 17:10:10 +0200

    Update tensorflow.py

--- wigs/tensorflow.py
+++ wigs/tensorflow.py
@@ -2,3 +2,4 @@ class tensorflow(Wig):
 	git_uri = 'https://github.com/tensorflow/tensorflow'
 	tarball_uri = 'https://github.com/tensorflow/tensorflow/archive/v{RELEASE_VERSION}.tar.gz'
 	last_release_version = '1.0.1'
+	dependencies = ['bazel', 'pip', 'numpy']
