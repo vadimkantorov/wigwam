9f13999f6d8f51ddf20faddef3c3943f48403b87
Vadim Kantorov <vadimkantorov@gmail.com>
2017-06-12 23:32:36 +0200

    Create cython.py

--- wigs/cython.py
+++ wigs/cython.py
@@ -1,4 +1,4 @@
 class cython(PythonWig):
 	tarball_uri = 'https://github.com/cython/cython/archive/{RELEASE_VERSION}.tar.gz'
-	last_release_version = '0.25b0'
+	last_release_version = '0.25.2'
 	git_uri = 'https://github.com/cython/cython'
