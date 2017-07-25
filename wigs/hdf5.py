99273e819172f4fcb3f9b34a6d0ba49bbb438b8e
Vadim Kantorov <vadimkantorov@gmail.com>
2017-07-25 17:15:27 +0200

    Update hdf5.py

--- wigs/hdf5.py
+++ wigs/hdf5.py
@@ -1,3 +1,3 @@
 class hdf5(Wig):
-	tarball_uri = 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-{RELEASE_VERSION}/src/hdf5-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '1.8.17'
+	tarball_uri = 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-{RELEASE_VERSION}/src/hdf5-{RELEASE_VERSION}.tar.gz'
+	last_release_version = '1.8.19'
