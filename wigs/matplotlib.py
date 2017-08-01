502b674ee17c80ca5cf8654bd7ba3876aa4ac523
Vadim Kantorov <vadimkantorov@gmail.com>
2017-08-01 16:37:03 +0200

    Update matplotlib.py

--- wigs/matplotlib.py
+++ wigs/matplotlib.py
@@ -2,4 +2,4 @@ class matplotlib(PythonWig):
 	tarball_uri = 'https://github.com/matplotlib/matplotlib/archive/v{RELEASE_VERSION}.tar.gz'
 	last_release_version = '2.0.0b4'
 	git_uri = 'https://github.com/matplotlib/matplotlib'
-	dependencies = ['numpy', 'freetype']
+	dependencies = ['numpy', 'freetype', 'pkg-config', 'libpng']
