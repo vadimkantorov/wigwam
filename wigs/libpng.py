e7105b0b69249246fd181ba980c92a98cf1991f5
Vadim Kantorov <vadimkantorov@gmail.com>
2017-08-01 16:42:00 +0200

    Update libpng.py

--- wigs/libpng.py
+++ wigs/libpng.py
@@ -1,4 +1,4 @@
 class libpng(Wig):
-	tarball_uri = 'ftp://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-{RELEASE_VERSION}.tar.gz'
+	tarball_uri = 'https://download.sourceforge.net/libpng/libpng-{RELEASE_VERSION}.tar.gz'
 	last_release_version = '1.6.30'
 	git_uri = 'git://git.code.sf.net/p/libpng/code'
