0852080710fa0045b249e9e08716ef010438b0b1
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:29:36 +0900

    hdf5

--- wigs/hdf5.py
+++ wigs/hdf5.py
@@ -1,3 +1,3 @@
 class hdf5(Wig):
-	tar_uri = 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-{RELEASE_VERSION}/src/hdf5-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '1.8.17'
+	tar_uri = 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-{VERSION}/src/hdf5-{VERSION}.tar.gz'
+	version = '1.8.17'
