a0b8afe8f9ae42e7010cda8ebabb8c499b7fa093
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:32:35 +0900

    octave

--- wigs/octave.py
+++ wigs/octave.py
@@ -1,3 +1,3 @@
 class octave(Wig):
-  tar_uri = 'ftp://ftp.gnu.org/gnu/octave/octave-{RELEASE_VERSION}.tar.gz'
-  last_release_version = '4.0.3'
+  tar_uri = 'ftp://ftp.gnu.org/gnu/octave/octave-{VERSION}.tar.gz'
+  version = '4.2.1'
