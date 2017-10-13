b506b9cb74a88e70a01aa2b21d6ac2b5dba29481
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 06:53:45 +0900

    openblas

--- wigs/openblas.py
+++ wigs/openblas.py
@@ -1,16 +1,14 @@
 class openblas(Wig):
-	tar_uri = 'https://github.com/xianyi/OpenBLAS/archive/v{RELEASE_VERSION}.tar.gz'
-	last_release_version = '0.2.19'
-	supported_features = ['debug', 'threads']
-	default_features = ['+threads']
+	tar_uri = 'https://github.com/xianyi/OpenBLAS/archive/v{version}.tar.gz'
+	version = '0.2.19'
 
-	def setup(self):
-		self.skip('configure')
-		self.make_flags += ['FC=gfortran', 'NO_AFFINITY=1']
-		self.make_install_flags += [S.PREFIX_MAKE_INSTALL_FLAG]
+	configure = None
+	make_flags = ['FC=gfortran', 'NO_AFFINITY=1']
+	make_install_flags = [S.PREFIX_MAKE_INSTALL_FLAG]
 	
 	def switch_debug_on(self):
 		self.make_flags += ['DEBUG=1']
 	
 	def switch_threads(self, on):
+		#default_features = ['+threads']
 		self.make_flags += ['USE_OPENMP=1'] if on else ['USE_OPENMP=0', 'USE_THREAD=0']
