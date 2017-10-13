d4fa9668c6ebffdc3eedd4a533f1e434ea54049c
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:32:51 +0900

    openssl

--- wigs/openssl.py
+++ wigs/openssl.py
@@ -1,11 +1,7 @@
 class openssl(Wig):
-	tar_uri = 'https://github.com/openssl/openssl/archive/OpenSSL_{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://github.com/openssl/openssl/archive/OpenSSL_{VERSION}.tar.gz'
 	git_uri = 'https://github.com/openssl/openssl'
-	last_release_version = '1_1_0e'
-	
-
-	def setup(self):
-		self.configure_flags += [S.FPIC_FLAG]
+	version = '1_1_0e'
 
 	def configure(self):
-		return './config %s' % ' '.join(self.configure_flags)
+		return './config {}'.format(' '.join([[S.PREFIX_CONFIGURE_FLAG, S.FPIC_FLAG]))
