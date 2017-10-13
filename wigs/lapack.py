650bd34bfe422005ded2a5488d094a12fff5f51d
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 02:17:51 +0900

    lapack

--- wigs/lapack.py
+++ wigs/lapack.py
@@ -1,3 +1,3 @@
 class lapack(Wig):
-	tar_uri = 'http://www.netlib.org/lapack/lapack-{RELEASE_VERSION}.tgz'
-	last_release_version = '3.5.0'
+	tar_uri = 'http://www.netlib.org/lapack/lapack-{VERSION}.tgz'
+	version = '3.5.0'
