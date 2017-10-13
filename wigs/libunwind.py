dbf3c0ce729e2584c3df8e28132492f4d809fde0
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:49:30 +0900

    linbunwind

--- wigs/libunwind.py
+++ wigs/libunwind.py
@@ -1,4 +1,4 @@
 class libunwind(Wig):
+	tar_uri = 'http://download.savannah.gnu.org/releases/libunwind/libunwind-{VERSION}.tar.gz'
 	git_uri = 'git://git.sv.gnu.org/libunwind.git'
-	tar_uri = 'http://download.savannah.gnu.org/releases/libunwind/libunwind-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '1.1'
+	version = '1.1'
