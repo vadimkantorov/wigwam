88716439175e4358ece0a9831eb240bbf2d91a5f
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:28:06 +0900

    libedit

--- wigs/libedit.py
+++ wigs/libedit.py
@@ -1,9 +1,7 @@
 class libedit(Wig):		
-	tar_uri = 'http://thrysoee.dk/editline/libedit-{RELEASE_VERSION}.tar.gz'		
-	last_release_version = '20160903-3.1'
+	tar_uri = 'http://thrysoee.dk/editline/libedit-{VERSION}.tar.gz'		
+	version = '20160903-3.1'
 	dependencies = ['ncurses']
-			
-	def setup(self):		
-		#self.configure_flags += ['--disable-shared']
-		self.before_configure += [S.export_prepend_paths(S.CPATH, ['$PREFIX/include/ncurses'])]
-		self.before_make += [S.export_prepend_paths(S.CPATH, ['$PREFIX/include/ncurses'])]
+	before_configure = [S.export_prepend_paths(S.CPATH, ['$PREFIX/include/ncurses'])]
+	before_make = [S.export_prepend_paths(S.CPATH, ['$PREFIX/include/ncurses'])]
+	#configure_flags = ['--disable-shared']
