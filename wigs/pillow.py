897d63ce38f961cb2fac1f0469e759a9d6fd4d66
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 02:29:41 +0900

    pillow

--- wigs/pillow.py
+++ wigs/pillow.py
@@ -1,5 +1,5 @@
 class pillow(PythonWig):
-	tar_uri = 'https://github.com/python-pillow/Pillow/archive/{RELEASE_VERSION}.tar.gz'
-	last_release_version = '3.4.1'
+	tar_uri = 'https://github.com/python-pillow/Pillow/archive/{VERSION}.tar.gz'
 	git_uri = 'https://github.com/python-pillow/Pillow'
+	version = '3.4.1'
 	dependencies = ['libjpeg']
