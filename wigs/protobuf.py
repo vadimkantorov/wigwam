968b988b25d69dd9db9af00ae1ebc127a375d181
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 06:42:23 +0900

    protobuf

--- wigs/protobuf.py
+++ wigs/protobuf.py
@@ -1,8 +1,7 @@
 class protobuf(AutogenWig):
-	tar_uri = 'https://github.com/google/protobuf/archive/v{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://github.com/google/protobuf/archive/v{version}.tar.gz'
 	git_uri = 'https://github.com/google/protobuf'
-	last_release_version = '3.1.0'
-	supported_features = ['python']
+	version = '3.1.0'
 	
 	def switch_python_on(self):
 		self.after_install += ['cd python', S.python_setup_install]
