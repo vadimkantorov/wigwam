415fb9708718f58b876ddb0dbe218b5ec057ae65
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 02:13:45 +0900

    matio

--- wigs/matio.py
+++ wigs/matio.py
@@ -1,5 +1,5 @@
 class matio(AutogenWig):
-  tar_uri = 'https://github.com/tbeu/matio/releases/download/v{RELEASE_VERSION}/matio-{RELEASE_VERSION}.tar.gz'
-  git_uri = 'https://github.com/tbeu/matio'
-  last_release_version = '1.5.10'
-  dependencies = ['zlib', 'hdf5', 'libtool', 'automake']
+	tar_uri = 'https://github.com/tbeu/matio/releases/download/v{VERSION}/matio-{VERSION}.tar.gz'
+	git_uri = 'https://github.com/tbeu/matio'
+	version = '1.5.10'
+	dependencies = ['zlib', 'hdf5', 'libtool', 'automake']
