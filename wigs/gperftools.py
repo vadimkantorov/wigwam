650d17f4e191c27d722e5af77bc95d31b3aa3fc5
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 01:12:32 +0900

    gperftools

--- wigs/gperftools.py
+++ wigs/gperftools.py
@@ -1,5 +1,5 @@
 class gperftools(Wig):
-	tar_uri = 'https://github.com/gperftools/gperftools/releases/download/gperftools-{RELEASE_VERSION}/gperftools-{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://github.com/gperftools/gperftools/releases/download/gperftools-{VERSION}/gperftools-{VERSION}.tar.gz'
 	git_uri = 'https://github.com/gperftools/gperftools'
-	last_release_version = '2.4'
+	version = '2.4'
 	dependencies = ['libunwind']
