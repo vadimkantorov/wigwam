955119dd2608c0a1cae08a768221a72a5a99d151
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:50:09 +0900

    fftw

--- wigs/fftw.py
+++ wigs/fftw.py
@@ -1,6 +1,4 @@
 class fftw(Wig):
-	tar_uri = 'http://www.fftw.org/fftw-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '3.3.4'
-
-	def setup(self):
-		self.configure_flags += ['--enable-sse2', '--enable-shared', '--with-pic', 'F77=gfortran']
+	tar_uri = 'http://www.fftw.org/fftw-{VERSION}.tar.gz'
+	version = '3.3.4'
+	configure_flags = ['--enable-sse2', '--enable-shared', '--with-pic', 'F77=gfortran']
