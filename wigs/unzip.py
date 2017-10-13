49fe598c0a5422991b4af933892cc2713d9b5a6b
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:17:28 +0900

    unzip

--- wigs/unzip.py
+++ wigs/unzip.py
@@ -1,9 +1,10 @@
 class unzip(Wig):
-	tar_uri = 'http://downloads.sourceforge.net/infozip/unzip{RELEASE_VERSION}.tar.gz'
-	last_release_version = '60'
-  
-	def setup(self):
-		self.skip('configure')
-		makefile_path = '"unix/Makefile"'
-		self.make_flags += ['-f', makefile_path, 'generic']
-		self.make_install_flags += [S.prefix_MAKE_INSTALL_FLAG, '-f', makefile_path]
+	tar_uri = 'http://downloads.sourceforge.net/infozip/unzip{VERSION}.tar.gz'
+	version = '60'
+	
+	makefile_path = '"unix/Makefile"'
+	make_flags += ['-f', makefile_path, 'generic']
+	make_install_flags += [S.prefix_MAKE_INSTALL_FLAG, '-f', makefile_path]
+ 
+	def configure(self):
+		return '' 
