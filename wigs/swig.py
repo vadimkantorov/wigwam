c6338973bff39046c20ebac05be3066600cf9eaf
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 01:07:22 +0900

    swig

--- wigs/swig.py
+++ wigs/swig.py
@@ -1,5 +1,5 @@
 class swig(AutogenWig):
-	tar_uri = 'https://github.com/swig/swig/archive/rel-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '3.0.10'
+	tar_uri = 'https://github.com/swig/swig/archive/rel-{VERSION}.tar.gz'
 	git_uri = 'https://github.com/swig/swig'
+	version = '3.0.10'
 	dependencies = ['autoconf', 'automake'] # libpcre, yacc/bison
