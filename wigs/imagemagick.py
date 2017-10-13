160dab60a4f955e4d3595bc3eabaa13e04d5162f
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:37:46 +0900

    imagemagick

--- wigs/imagemagick.py
+++ wigs/imagemagick.py
@@ -1,3 +1,3 @@
 class imagemagick(Wig):
-	tar_uri = 'http://www.imagemagick.org/download/ImageMagick.tar.gz'
-	last_release_version = '6.9.1'
+	tar_uri = 'http://www.imagemagick.org/download/ImageMagick-{VERSION}.tar.gz'
+	version = '7.0.7-7'
