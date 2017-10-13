149b0a2ca90a9ee10d5a5bb55cefb290db754876
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:40:51 +0900

    libjpegturbo

--- wigs/libjpegturbo.py
+++ wigs/libjpegturbo.py
@@ -1,5 +1,5 @@
 class libjpegturbo(Wig):
+	tar_uri = 'https://github.com/libjpeg-turbo/libjpeg-turbo/archive/{VERSION}.tar.gz'
 	git_uri = 'https://github.com/libjpeg-turbo/libjpeg-turbo'
-	tar_uri = 'https://github.com/libjpeg-turbo/libjpeg-turbo/archive/{RELEASE_VERSION}.tar.gz'
-	last_release_version = '1.5.1'
+	version = '1.5.1'
 	dependencies = ['autoconf', 'automake', 'libtool', 'yasm']
