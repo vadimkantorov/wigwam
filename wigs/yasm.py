da250876ae5775e6d87788958c6e260d01ed02db
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:43:32 +0900

    yasm

--- wigs/yasm.py
+++ wigs/yasm.py
@@ -1,4 +1,4 @@
 class yasm(Wig):
 	tar_uri = 'http://www.tortall.net/projects/yasm/releases/yasm-{VERSION}.tar.gz'
-	git_uri = 'https://github.com/yasm/yasm'	
+	git_uri = 'https://github.com/yasm/yasm'
 	version = '1.3.0'
