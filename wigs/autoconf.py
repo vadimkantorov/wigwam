40b1518d473f6f709ae3b624ed99a6608659df75
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:41:25 +0900

    autoconf

--- wigs/autoconf.py
+++ wigs/autoconf.py
@@ -1,4 +1,4 @@
 class autoconf(Wig):
+	tar_uri = 'http://ftp.gnu.org/gnu/autoconf/autoconf-{VERSION}.tar.gz'
 	git_uri = 'git://git.sv.gnu.org/autoconf'
-	tar_uri = 'http://ftp.gnu.org/gnu/autoconf/autoconf-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '2.69'
+	version = '2.69'
