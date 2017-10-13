4063ed842da69a7da1c9917ac96ce41c04191721
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:38:54 +0900

    libtool

--- wigs/libtool.py
+++ wigs/libtool.py
@@ -1,4 +1,4 @@
 class libtool(Wig):
-	tar_uri = 'http://ftpmirror.gnu.org/libtool/libtool-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '2.4.6'
+	tar_uri = 'http://ftpmirror.gnu.org/libtool/libtool-{VERSION}.tar.gz'
+	version = '2.4.6'
 	dependencies = ['m4']
