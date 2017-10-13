2308ef5572e6750e21b992d3f2a4e15d22282ae4
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 05:32:57 +0900

    magma

--- wigs/magma.py
+++ wigs/magma.py
@@ -1,15 +1,13 @@
 class magma(Wig):
-	tar_uri = 'http://icl.cs.utk.edu/projectsfiles/magma/downloads/magma-{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'http://icl.cs.utk.edu/projectsfiles/magma/downloads/magma-{VERSION}.tar.gz'
+	release_version = '1.6.2'
 	dependencies = ['openblas']
-	last_release_version = '1.6.2'
 	config_access = ['PATH_TO_NVCC']
+	make_install_flags = [S.prefix_MAKE_INSTALL_FLAG]
 	
-	def setup(self):
-		self.make_install_flags += [S.prefix_MAKE_INSTALL_FLAG]
-
 	def configure(self):
 		config_fixes = '''| sed 's$#OPENBLASDIR ?=$OPENBLASDIR ?= '$PREFIX'#OPENBLASDIR ?=$' '''
-		config_fixes += '''| sed 's$#CUDADIR ?=$CUDADIR ?= '%s'#CUDADIR ?=$' ''' % os.path.join(os.path.dirname(self.getenv('PATH_TO_NVCC')), '..')
+		config_fixes += '''| sed 's$#CUDADIR ?=$CUDADIR ?= '{}'#CUDADIR ?=$' '''.format(os.path.join(os.path.dirname(self.getenv('PATH_TO_NVCC')), '..'))
 		config_fixes += '''| sed '$aLIB += -lm' '''
 		config_fixes += '''| sed '$aNVCC = $(CUDADIR)/bin/nvcc' '''
-		return ['cat make.inc.openblas %s > make.inc' % config_fixes]
+		return 'cat make.inc.openblas {} > make.inc'.format(config_fixes)
