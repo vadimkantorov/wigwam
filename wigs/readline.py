47869c1e1e427d4dc6a668d692a365d8300b1777
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:52:50 +0900

    readline

--- wigs/readline.py
+++ wigs/readline.py
@@ -1,8 +1,6 @@
 class readline(Wig):
-	tar_uri = 'http://ftp.gnu.org/gnu/readline/readline-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '6.3'
-
-	def setup(self):
-		self.configure_flags += ['--disable-shared']
-		self.before_configure += [S.export(S.CFLAGS, '-fPIC')]
-		self.before_make += [S.export(S.CFLAGS, '-fPIC')]
+	tar_uri = 'http://ftp.gnu.org/gnu/readline/readline-{VERSION}.tar.gz'
+	version = '6.3'
+	configure_flags = ['--disable-shared']
+	before_configure = [S.export(S.CFLAGS, '-fPIC')]
+	before_make = [S.export(S.CFLAGS, '-fPIC')]
