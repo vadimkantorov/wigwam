57030350752bf350a0cca09397acc4a3a978cd70
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:26:39 +0900

    lmdb

--- wigs/lmdb.py
+++ wigs/lmdb.py
@@ -1,10 +1,10 @@
 class lmdb(Wig):
-	tar_uri = 'https://github.com/LMDB/lmdb/archive/LMDB_{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://github.com/LMDB/lmdb/archive/LMDB_{VERSION}.tar.gz'
 	git_uri = 'https://github.com/LMDB/lmdb'
+	version = '0.9.15'
 	working_directory = 'libraries/liblmdb'
-	last_release_version = '0.9.15'
+	before_install = [S.mkdir_p('$PREFIX/man')]
+	make_install_flags = ['prefix="$PREFIX"']
 
-	def setup(self):
-		self.skip('configure')
-		self.before_install += [S.mkdir_p('$PREFIX/man')]
-		self.make_install_flags += ['prefix="$PREFIX"']
+	def configure(self):
+		return ''
