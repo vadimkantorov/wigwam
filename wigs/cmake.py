91cee56999c1a5ee2fc390436511652d27a0a3a4
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 02:38:30 +0900

    cmake

--- wigs/cmake.py
+++ wigs/cmake.py
@@ -1,6 +1,6 @@
 class cmake(Wig):
-	tar_uri = 'https://cmake.org/files/v3.5/cmake-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '3.5.1'
+	tar_uri = 'https://cmake.org/files/v3.5/cmake-{VERSION}.tar.gz'
+	version = '3.5.1'
 
 	def configure(self):
-		return ['./bootstrap %s' % ' '.join(self.configure_flags)]
+		return './bootstrap {}'.format(' '.join(self.configure_flags))
