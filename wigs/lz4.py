9f6516199280c80da253fd37493cfb3682b95127
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 04:23:35 +0900

    lz4

--- wigs/lz4.py
+++ wigs/lz4.py
@@ -2,7 +2,7 @@ class lz4(Wig):
 	tar_uri = 'https://github.com/Cyan4973/lz4/archive/r{VERSION}.tar.gz'
 	git_uri = 'https://github.com/Cyan4973/lz4'
 	version = '130'
-	make_install_flags += [S.PREFIX_MAKE_INSTALL_FLAG]
+	make_install_flags = [S.PREFIX_MAKE_INSTALL_FLAG]
 
 	def configure(self):
 		return ''
