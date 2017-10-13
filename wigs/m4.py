4fd66956679bc000a2398f8ce7466fd4374452fb
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:30:19 +0900

    m4

--- wigs/m4.py
+++ wigs/m4.py
@@ -1,4 +1,4 @@
 class m4(Wig):
-	tar_uri = 'https://ftp.gnu.org/gnu/m4/m4-{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://ftp.gnu.org/gnu/m4/m4-{VERSION}.tar.gz'
 	git_uri = 'http://git.savannah.gnu.org/r/m4.git'
-	last_release_version = '1.4.17'
+	version = '1.4.17'
