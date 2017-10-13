6ea82a7ab802d7a52907fad54d57fdf400e2fefc
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 01:20:40 +0900

    leveldb

--- wigs/leveldb.py
+++ wigs/leveldb.py
@@ -1,10 +1,10 @@
 class leveldb(Wig):
-	tar_uri = 'https://github.com/google/leveldb/archive/v{RELEASE_VERSION}.tar.gz'
+	tar_uri = 'https://github.com/google/leveldb/archive/v{VERSION}.tar.gz'
 	git_uri = 'https://github.com/google/leveldb'
-	last_release_version = '1.18'
+	version = '1.18'
 
-	def setup(self):
-		self.skip('configure')
+	def configure(self):
+		return ''
 
 	def install(self):
 		return ['cp --preserve=links libleveldb.* "$PREFIX/lib"',
