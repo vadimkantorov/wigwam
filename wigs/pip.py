2c0ed50ce33157a0a5f8b1ffee2f098997914369
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 02:42:15 +0900

    pip

--- wigs/pip.py
+++ wigs/pip.py
@@ -1,12 +1,12 @@
 class pip(Wig):
+	uri = 'https://bootstrap.pypa.io/get-pip.py'
 	git_uri = 'https://github.com/pypa/get-pip'
-	raw_uri = 'https://bootstrap.pypa.io/get-pip.py'
 
-	def setup(self):
-		self.skip('configure', 'make')
+	def configure(self):
+		return ''
 
-	def fetch(self):
-		return [S.mkdir_p(self.paths.src_dir), S.download(self.raw_uri, os.path.join(self.paths.src_dir, os.path.basename(self.raw_uri)))]
-	
+	def build(self):
+		return ''
+
 	def install(self):
-		return [S.export('PYTHONUSERBASE', S.PREFIX_PYTHON), 'python "%s" --force-reinstall --ignore-installed --user pip setuptools wheel ' % os.path.join(self.paths.src_dir, os.path.basename(self.raw_uri))]
+		return [S.export('PYTHONUSERBASE', S.PREFIX_PYTHON), 'python "{}" --force-reinstall --ignore-installed --user pip setuptools wheel'.format(os.path.join(self.paths.src_dir, os.path.basename(self.uri)))]
