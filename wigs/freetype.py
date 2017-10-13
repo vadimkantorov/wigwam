5f4f67b973178debf9f84d7613ff00ccd4731763
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 01:30:59 +0900

    freetype

--- wigs/freetype.py
+++ wigs/freetype.py
@@ -1,3 +1,3 @@
 class freetype(Wig):
-	tar_uri = 'http://download.savannah.gnu.org/releases/freetype/freetype-{RELEASE_VERSION}.tar.gz'
-	last_release_version = '2.4.0'
+	tar_uri = 'http://download.savannah.gnu.org/releases/freetype/freetype-{VERSION}.tar.gz'
+	version = '2.4.0'
