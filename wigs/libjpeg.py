9d66b3c242291ee857f16195e0e28cd552af5522
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 00:50:49 +0900

    libjpeg

--- wigs/libjpeg.py
+++ wigs/libjpeg.py
@@ -1,3 +1,3 @@
 class libjpeg(Wig):
-  tar_uri = 'http://www.ijg.org/files/jpegsrc.v{RELEASE_VERSION}.tar.gz'
-  last_release_version = '9b'
+	tar_uri = 'http://www.ijg.org/files/jpegsrc.v{VERSION}.tar.gz'
+	version = '9b'
