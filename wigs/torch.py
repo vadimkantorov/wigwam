a5414e48f233688953689fa85749467334510f7f
Vadim Kantorov <vadimkantorov@gmail.com>
2017-10-14 10:01:11 +0900

    ...

--- wigs/torch.py
+++ wigs/torch.py
@@ -1,11 +1,7 @@
 class torch(CmakeWig):
 	git_uri = 'https://github.com/torch/distro'
-	git_init_submodules = True
 	dependencies = ['openblas', 'readline', 'ncurses', 'gnuplot', 'libjpeg', 'unzip', 'cmake']
-	optional_dependencies = ['magma']
-	config_access = ['PATH_TO_NVCC', 'PATH_TO_CUDNN_SO']
-	supported_features = ['qt', 'cuda', 'cudnn']
-	default_features = ['+cuda', '+qt', '+cudnn']
+	#default_features = ['+cuda', '+qt', '+cudnn']
 	
 	@staticmethod
 	def luarocks_make(rockspec_path):
@@ -15,39 +11,36 @@ class torch(CmakeWig):
 		rockspec_path = os.path.join(*splitted[2:])
 		return '( cd "%s"; %s )' % (working_directory, '; '.join(LuarocksWig(pkg_name, rockspec_path).install()))
 
-	def setup(self):
-		self.cmake_flags += ['-DWITH_LUAJIT21=ON', '-DLIBS="-lreadline -lncurses"']
+	cmake_flags = ['-DWITH_LUAJIT21=ON', '-DLIBS="-lreadline -lncurses"']
+	after_install = [S.CD_PARENT] + map(torch.luarocks_make, [
+		# Installing common lua packages
+		'extra/luafilesystem/rockspecs/luafilesystem-1.6.3-1.rockspec',
+		'extra/penlight/penlight-scm-1.rockspec',
+		'extra/lua-cjson/lua-cjson-2.1devel-1.rockspec',
 		
-		self.after_install += [S.CD_PARENT]
-		self.after_install += map(torch.luarocks_make, [
-			# Installing common lua packages
-			'extra/luafilesystem/rockspecs/luafilesystem-1.6.3-1.rockspec',
-			'extra/penlight/penlight-scm-1.rockspec',
-			'extra/lua-cjson/lua-cjson-2.1devel-1.rockspec',
-			
-			# Installing core Torch packages
-			'extra/luaffifb/luaffi-scm-1.rockspec',
-			'pkg/sundown/rocks/sundown-scm-1.rockspec',
-			'pkg/cwrap/rocks/cwrap-scm-1.rockspec',
-			'pkg/paths/rocks/paths-scm-1.rockspec',
-			'pkg/torch/rocks/torch-scm-1.rockspec',
-			'pkg/dok/rocks/dok-scm-1.rockspec',
-			'exe/trepl/trepl-scm-1.rockspec',
-			'pkg/sys/sys-1.1-0.rockspec',
-			'pkg/xlua/xlua-1.0-0.rockspec',
-			'extra/nn/rocks/nn-scm-1.rockspec',
-			'extra/graph/rocks/graph-scm-1.rockspec',
-			'extra/nngraph/nngraph-scm-1.rockspec',
-			'pkg/image/image-1.1.alpha-0.rockspec',
-			'pkg/optim/optim-1.0.5-0.rockspec',
-			
-			# Installing optional Torch packages
-			'pkg/gnuplot/rocks/gnuplot-scm-1.rockspec',
-			'exe/env/env-scm-1.rockspec',
-			'extra/nnx/nnx-0.1-1.rockspec',
-			'extra/threads/rocks/threads-scm-1.rockspec',
-			'extra/argcheck/rocks/argcheck-scm-1.rockspec',
-		])
+		# Installing core Torch packages
+		'extra/luaffifb/luaffi-scm-1.rockspec',
+		'pkg/sundown/rocks/sundown-scm-1.rockspec',
+		'pkg/cwrap/rocks/cwrap-scm-1.rockspec',
+		'pkg/paths/rocks/paths-scm-1.rockspec',
+		'pkg/torch/rocks/torch-scm-1.rockspec',
+		'pkg/dok/rocks/dok-scm-1.rockspec',
+		'exe/trepl/trepl-scm-1.rockspec',
+		'pkg/sys/sys-1.1-0.rockspec',
+		'pkg/xlua/xlua-1.0-0.rockspec',
+		'extra/nn/rocks/nn-scm-1.rockspec',
+		'extra/graph/rocks/graph-scm-1.rockspec',
+		'extra/nngraph/nngraph-scm-1.rockspec',
+		'pkg/image/image-1.1.alpha-0.rockspec',
+		'pkg/optim/optim-1.0.5-0.rockspec',
+		
+		# Installing optional Torch packages
+		'pkg/gnuplot/rocks/gnuplot-scm-1.rockspec',
+		'exe/env/env-scm-1.rockspec',
+		'extra/nnx/nnx-0.1-1.rockspec',
+		'extra/threads/rocks/threads-scm-1.rockspec',
+		'extra/argcheck/rocks/argcheck-scm-1.rockspec',
+	])
 	
 	def switch_qt_on(self):
 		self.after_install += map(torch.luarocks_make, [
