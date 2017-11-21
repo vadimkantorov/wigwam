8944053c93c38b5ad45e3cdb9c477b004d6841d0
Vadim Kantorov <vadimkantorov@gmail.com>
2017-11-22 01:44:01 +0900

    ...

--- wigs/pytorch.py
+++ wigs/pytorch.py
@@ -9,3 +9,7 @@ class pytorch(PythonWig):
 			self.require('magma')
 		else:
 			self.before_install += [S.export('NO_CUDA', '1')]
+
+	def switch_cudnn(self, on):
+		if on:
+			self.before_install += [S.export('CUDNN_LIBRARY', os.path.dirname(self.getenv('PATH_TO_CUDNN_SO'))), S.export('CUDNN_INCLUDE_DIR',  os.path.join(os.path.dirname(self.getenv('PATH_TO_CUDNN_SO')), '../include'))]
