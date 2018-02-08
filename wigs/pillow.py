d073b6ee04a03c8b1c9d2b2090cf20a03281b731
Vadim Kantorov <vadimkantorov@gmail.com>
2018-02-08 16:45:26 +0100

    Update pillow.py

--- wigs/pillow.py
+++ wigs/pillow.py
@@ -2,4 +2,4 @@ class pillow(PythonWig):
 	tarball_uri = 'https://github.com/python-pillow/Pillow/archive/{RELEASE_VERSION}.tar.gz'
 	last_release_version = '3.4.1'
 	git_uri = 'https://github.com/python-pillow/Pillow'
-	dependencies = ['libjpeg']
+	dependencies = ['libjpeg', 'zlib']
