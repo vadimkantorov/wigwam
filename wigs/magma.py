0ed383c18aa21855846d63e3f805d63ef38653da
Vadim Kantorov <vadimkantorov@gmail.com>
2018-02-12 17:03:14 +0100

    Update magma.py

--- wigs/magma.py
+++ wigs/magma.py
@@ -12,6 +12,4 @@ class magma(Wig):
 		config_fixes += '''| sed 's$#CUDADIR ?=$CUDADIR ?= '%s'#CUDADIR ?=$' ''' % os.path.join(os.path.dirname(self.cfg('PATH_TO_NVCC')), '..')
 		config_fixes += '''| sed '$aLIB += -lm' '''
 		config_fixes += '''| sed '$aNVCC = $(CUDADIR)/bin/nvcc' '''
-		config_fixes += '''| sed '$aCFLAGS += -D_GLIBCXX_USE_C99 -D_GLIBCXX_USE_C99_MATH' '''
-		config_fixes += '''| sed '$aCXXFLAGS += -D_GLIBCXX_USE_C99 -D_GLIBCXX_USE_C99_MATH' '''
 		return ['cat make.inc-examples/make.inc.openblas %s > make.inc' % config_fixes]
