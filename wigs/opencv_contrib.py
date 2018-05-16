64eea203a9088bedab7c11aa440cb2abd19e91a6
Vadim Kantorov <vadimkantorov@gmail.com>
2018-05-16 18:30:39 +0200

    Update opencv_contrib.py

--- wigs/opencv_contrib.py
+++ wigs/opencv_contrib.py
@@ -1,5 +1,7 @@
 class opencv_contrib(Wig):
 	git_uri = 'https://github.com/opencv/opencv_contrib'
+	tarball_uri = 'https://github.com/opencv/opencv_contrib/archive/{RELEASE_VERSION}.tar.gz'
+	last_release_version = '3.3.0'
 
 	def setup(self):
 		self.skip('configure', 'make', 'install')
