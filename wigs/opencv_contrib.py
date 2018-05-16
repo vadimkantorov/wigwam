b7091b0f3f5f41b95298d392821c47dcf09d7699
Vadim Kantorov <vadimkantorov@gmail.com>
2018-05-16 18:20:49 +0200

    Update opencv_contrib.py

--- wigs/opencv_contrib.py
+++ wigs/opencv_contrib.py
@@ -1,5 +1,5 @@
 class opencv_contrib(Wig):
-	git_uri = 'https://github.com/itseez/opencv_contrib'
+	git_uri = 'https://github.com/opencv/opencv_contrib'
 
 	def setup(self):
 		self.skip('configure', 'make', 'install')
